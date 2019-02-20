21c4bec637b909edc8eaabd6bb4c50e05f6164c7
Avi5006 <47788562+Avi5006@users.noreply.github.com>
2019-02-20 11:41:38 -0500

    Update Pythagorean-theorem.js

--- Pythagorean-theorem.js
+++ Pythagorean-theorem.js
@@ -1,11 +1,15 @@
 
 import java.util.Scanner;
+
 class apples{
     public static void main(String args[]){
         Scanner input = new Scanner(System.in);
+        // var
         double x, y;
+        // putting in the inputs
         x = input.nextDouble();
         y = input.nextDouble();
+        //out puts               roots    power by 2           power by 2                     
         System.out.println(Math.sqrt(Math.pow(x, 2)+ Math.pow(y, 2)));
     }
 }
