05250292fcc9d85cdfb43dd45cc31c976ce57a4c
Avi5006 <47788562+Avi5006@users.noreply.github.com>
2019-02-20 11:58:27 -0500

    Update mid-lips.js

--- mid-lips.js
+++ mid-lips.js
@@ -1,15 +1,21 @@
+// imports scanner to use 
 import java.util.Scanner;
 public class MadLib { 
     public static void main(String[] args){
-
+           //this 
         Scanner i = new Scanner(System.in);
+        //my two strings
         String noun, noun2;
+        //system asking user to enter 
         System.out.println("Enter a noun ");
+        inputs
         noun = i.nextLine();
+        //asking user to input a 2nd one 
         System.out.println("Enter a second noun: ");
+        //inputs
         noun2 = i.nextLine();
        
-     
+     //out puts there two noun they give
         System.out.println("Mary had a little " + noun + "\nIts " + noun2 + " was silly as snow");
         System.out.println("And everywhere that Mary went");
         System.out.println("The " + noun + " was sure to go.");
